8eb78c07e95b6e7bb141271f613e83eb1a805ae3
QuantumTech <103763740+roycuadra@users.noreply.github.com>
2023-11-05 17:12:33 +0800

    Update Home Automation.ino

--- Home Automation.ino
+++ Home Automation.ino
@@ -1,18 +1,35 @@
+/*
+................................................
+:   Title:Simple Home Automation               :
+:   Created by: Roy Cuadra                     :
+:   Date: 05/11/2023                           :
+................................................
+
+*/
+
+//remove or comment this when using esp8266
 #include <WiFi.h>
+
+//Uncomment this when using esp8266 
 // #include <ESP8266WiFi.h>
 
-const char* ssid = "Home-Control";
+const char* ssid = "IoT Home Automation";
 const char* password = "admin12345";
+
 WiFiServer server(80);
 String header;
-String output5State = "off"; // Variable to store the state of GPIO5
-const int output5 = 5; // Define GPIO5 as the output pin
+String output5State = "off";
+
+const int output5 = 12; // For ESP32: GPIO 12, For ESP8266: D6
+
+const int buzzerPin = 13; //for ESP32: GPIO 13, for esp8266 D6 pin to which the buzzer is connected
 
 void setup() {
   Serial.begin(115200);
-  pinMode(output5, OUTPUT); // Initialize GPIO5 as an output
-  digitalWrite(output5, HIGH); // Set GPIO5 to HIGH (off) initially
-
+  pinMode(output5, OUTPUT);
+  digitalWrite(output5, HIGH);
+  pinMode(buzzerPin, OUTPUT);
+  
   Serial.print("Setting AP (Access Point)...");
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
@@ -40,29 +57,32 @@ void loop() {
             client.println("Connection: close");
             client.println();
 
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
+            if (header.indexOf("GET /12/on") >= 0) {
+              Serial.println("GPIO 12 on");
               output5State = "on";
               digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
+              beep(); 
+            } else if (header.indexOf("GET /12/off") >= 0) {
+              Serial.println("GPIO 12 off");
               output5State = "off";
               digitalWrite(output5, HIGH);
+              beep();
             }
 
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;background: rgb(2,0,36); background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);}");
+            client.println(".button { background-color:blue; border: none;border-radius:30px; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
+            client.println("body{ padding-top:10px; padding-bottom:10px; border-radius: 20px; border: 1px solid rgb(255, 255, 255); border-style: none; box-shadow: 0 0 30px 0 rgb(71, 70, 70); margin-top: 100px; max-width: 300px ; margin-left: auto; margin-right: auto; background-color: white;  }");
+            client.println(".button2 {background-color:red;}</style></head>");
             client.println("<body><h1>Control Panel</h1>");
 
             if (output5State == "off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+              client.println("<p><a href=\"/12/on\"><button class=\"button\">ON</button></a></p>");
             } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+              client.println("<p><a href=\"/12/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
 
             client.println("</body></html>");
@@ -81,4 +101,10 @@ void loop() {
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+}
+
+void beep() {
+  digitalWrite(buzzerPin, HIGH); 
+  delay(100); 
+  digitalWrite(buzzerPin, LOW);
 }
